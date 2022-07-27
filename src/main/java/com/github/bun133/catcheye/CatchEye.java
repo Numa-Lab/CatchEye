31b04379e371bb9c6446a6eafe7e73ebebae2923
bun133 <bun133.work@gmail.com>
2022-07-27 21:02:40 +0900

    Update頻度を2倍に

--- src/main/java/com/github/bun133/catcheye/CatchEye.java
+++ src/main/java/com/github/bun133/catcheye/CatchEye.java
@@ -38,22 +38,37 @@ public class CatchEye {
     public static Vector3d nowLocation = null;
     public static Vector3d lastLocation = null;
     private static Long lastTime = null;
-    private static int lastTicks = 0;
     public static final double one_tick_length_in_mill = 1000.0 / 20.0;
+    public static final double update_interval_in_tick = 10.0;
 
-    public static void pushLocation(Vector3d newLocation, int ticks) {
+    public static void pushLocation(Vector3d newLocation) {
         lastLocation = nowLocation;
         nowLocation = newLocation;
         lastTime = Util.milliTime();
-        lastTicks = ticks;
+    }
+
+    public static int getDeltaTick() {
+        if (lastTime == null) {
+            return 0; // 初期化前は0.0ということで
+        }
+        return (int) Math.ceil((Util.milliTime() - lastTime) / one_tick_length_in_mill);
     }
 
     public static double getPartial() {
         if (lastTime == null) {
             return 0.0; // 初期化前は0.0ということで
         }
-        double deltaTick = (Util.milliTime() - lastTime) / one_tick_length_in_mill;
-        return deltaTick / (double) lastTicks;
+        double d = (Util.milliTime() - lastTime) / one_tick_length_in_mill;
+        int i = (int) Math.ceil(d);
+        return Math.max(Math.min(d - (double) i, 1.0), 0.0);
+    }
+
+    public static Vector3d getLerpVec(Vector3d from, Vector3d to, double pst) {
+        return new Vector3d(
+                from.x + (to.x - from.x) * pst,
+                from.y + (to.y - from.y) * pst,
+                from.z + (to.z - from.z) * pst
+        );
     }
 
     @SubscribeEvent
@@ -70,27 +85,44 @@ public class CatchEye {
             return;
         }
 
-        double partialTicks = getPartial();
+        double partialTicks = e.renderTickTime;
         Optional<? extends PlayerEntity> target = clientPlayer.world.getPlayers().stream().filter((p) -> p.getGameProfile().getName().equals(CatchEye.target_name)).findFirst();
         if (!target.isPresent()) {
             if (target_name.isEmpty()) return;
-
-            if (nowLocation != null) {
-                clientPlayer.lookAt(EntityAnchorArgument.Type.FEET, new Vector3d(
-                        MathHelper.lerp(partialTicks, lastLocation.x, nowLocation.x),
-                        MathHelper.lerp(partialTicks, lastLocation.y, nowLocation.y),
-                        MathHelper.lerp(partialTicks, lastLocation.z, nowLocation.z)
-                ));
-//                lookAt(lastLocation, nowLocation, clientPlayer, partialTicks / 20.0);
+            if (nowLocation != null && lastLocation != null) {
+                vanillaLookAt(getLerpVec(lastLocation, nowLocation, getDeltaTick() / update_interval_in_tick), getLerpVec(lastLocation, nowLocation, (getDeltaTick() + 1) / update_interval_in_tick), clientPlayer, getPartial());
             }
             return;
         }
 
-        partialTicks = e.renderTickTime;
-
         PlayerEntity real_target = target.get();
-        pushLocation(new Vector3d(real_target.lastTickPosX, real_target.lastTickPosY, real_target.lastTickPosZ), 1);
+        pushLocation(new Vector3d(real_target.lastTickPosX, real_target.lastTickPosY, real_target.lastTickPosZ));
         lookAt(nowLocation, real_target.getPositionVec(), clientPlayer, partialTicks);
+    }
+
+    private void vanillaLookAt(Vector3d from, Vector3d to, PlayerEntity player, double partialTick) {
+        Vector3d fromPoint = player.getPositionVec().add(0, player.getEyeHeight(), 0);
+        vanillaLookAt(
+                fromPoint,
+                new Vector3d(
+                        MathHelper.lerp(partialTick, from.x, to.x),
+                        MathHelper.lerp(partialTick, from.y, to.y),
+                        MathHelper.lerp(partialTick, from.z, to.z)
+                ),
+                player
+        );
+    }
+
+    private void vanillaLookAt(Vector3d from, Vector3d to, PlayerEntity player) {
+        double d0 = to.x - from.x;
+        double d1 = to.y - from.y;
+        double d2 = to.z - from.z;
+        double d3 = MathHelper.sqrt(d0 * d0 + d2 * d2);
+        player.rotationPitch = MathHelper.wrapDegrees((float) (-(MathHelper.atan2(d1, d3) * (double) (180F / (float) Math.PI))));
+        player.rotationYaw = MathHelper.wrapDegrees((float) (MathHelper.atan2(d2, d0) * (double) (180F / (float) Math.PI)) - 90.0F);
+        player.setRotationYawHead(player.rotationYaw);
+        player.prevRotationPitch = player.rotationPitch;
+        player.prevRotationYaw = player.rotationYaw;
     }
 
     private void lookAt(Vector3d lastTickPos, Vector3d nowPos, ClientPlayerEntity clientPlayer, double partialTicks) {
@@ -116,7 +148,7 @@ public class CatchEye {
         CatchEye.target_name = message.getTarget();
         Vector3d position = message.getPosition();
         if (position != null && !target_name.isEmpty()) {
-            pushLocation(position, 20);
+            pushLocation(position);
         }
         CatchEye.LOGGER.info("Update target: " + message.getTarget());
         ctx.get().setPacketHandled(true);
